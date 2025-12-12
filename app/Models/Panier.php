1a7af1ed3b126ab11c275ac27c65cea0dd8e7d36
kakotodev <vinh-lam.le@efrei.net>
2025-12-12 13:30:32 +0100

    adding Panier
    Ajout du fichier Panier.php

---
+++ app/Models/Panier.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Mini\Models;
+
+use Mini\Core\Database;
+use PDO;
+
+class Panier{
+
+    private $id;
+    private $utilisateur_id;
+    private $produit_id;
+    private $status;
+
+    public function getId(){
+        return $this->id;
+    }
+
+    public function setId($id){
+        $this->id = $id;
+    }
+
+    public function getUtilisateurID(){
+        return $this->utilisateur_id;
+    }
+
+    public function setUtilisateurID($utilisateur_id){
+        $this->utilisateur_id= $utilisateur_id;
+    }
+
+    public function getProduitId(){
+        return $this->produit_id;
+    }
+
+    public function setProduitId($produit_id){
+        $this->produit_id = $produit_id;
+    }
+
+    public function getStatus(){
+        return $this->status;
+    }
+
+    public function setStatus($status){
+        $this->status = $status;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getAll(){
+        $pdo = Database::getPDO();
+        $stmt = $pdo-query("SELECT * FROM panier ORDER BY id ASC");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @param int
+     * @return array|null
+     */
+    public static function selectByID(){
+        $pdo = Databse::getPDO();
+        $stmt = $pdo->query("SELECT * FROM panier BY ?");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
